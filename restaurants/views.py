c0a23043cb7b27a5e672c87b5496cce891714a91
matt-smrke <matthew.smrke@empire.ca>
2018-01-29 12:33:46 -0500

    adds templates

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,8 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
 
 # Create your views here.
 def home(request):
-    return HttpResponse('hello karl...')
-    # return render(request, 'template', {})
-    
+    return render(request, 'home.html', {"myvar": "Slippery Pete"})
